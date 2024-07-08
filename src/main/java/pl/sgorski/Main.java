68adfb09ac695a6a8eb7d366381e63e3ed7de7b3
Sebastian Górski <sebastian.gorski@studenci.collegiumwitelona.pl>
2024-07-08 20:35:42 +0200

    login window first touch

--- src/main/java/pl/sgorski/Main.java
+++ src/main/java/pl/sgorski/Main.java
@@ -1,9 +1,13 @@
 package pl.sgorski;
 
+import pl.sgorski.ui.LoginScreen;
+
 public class Main {
     //https://www.zenrows.com/blog/web-scraping-java#install-jsoup
     //https://docs.oracle.com/javase%2Ftutorial%2Fuiswing%2F%2F/components/frame.html
     public static void main(String[] args) {
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.frame();
         System.out.println("Hello world!");
     }
 }
--- src/main/java/pl/sgorski/ui/LoginScreen.java
+++ src/main/java/pl/sgorski/ui/LoginScreen.java
@@ -1,12 +1,39 @@
 package pl.sgorski.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginScreen {
     private final JFrame mainFrame = new JFrame();
+    private final JTextField loginField = new JTextField();
+    private final JTextField passwordField = new JTextField();
+    private final GridLayout gridLayout = new GridLayout(7, 1);
+    private final FlowLayout bottomPanel = new FlowLayout();
+    private final JButton loginButton = new JButton();
+    private final JButton exitButton = new JButton();
+
+    private final Dimension textFieldDim = new Dimension(50, 100);
 
     public void frame() {
+        loginField.setPreferredSize(textFieldDim);
+        passwordField.setPreferredSize(textFieldDim);
+
+        bottomPanel.addLayoutComponent("exit", exitButton);
+        bottomPanel.addLayoutComponent("login", loginButton);
+
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(250, 250);
+        mainFrame.setPreferredSize(new Dimension(250, 250));
+        mainFrame.setMinimumSize(new Dimension(250, 250));
+        mainFrame.setResizable(false);
+        mainFrame.setLayout(gridLayout);
+        mainFrame.getContentPane().add(new JLabel("Wybierz świat:"));
+        mainFrame.getContentPane().add(new JOptionPane());
+        mainFrame.getContentPane().add(new JLabel("Login:"));
+        mainFrame.getContentPane().add(loginField);
+        mainFrame.getContentPane().add(new JLabel("Password:"));
+        mainFrame.getContentPane().add(passwordField);
+        mainFrame.getContentPane().add(bottomPanel);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
     }
 }
